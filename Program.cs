dd1200526fa90a3ed2fa0e751cc478ae3119696c
Andrew Robilliard <robilliardnz@gmail.com>
2021-07-06 07:46:50 +1200

    🎉 (global): initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace devutil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
